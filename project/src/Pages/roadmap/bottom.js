510118c8f91597af3f9de520d46fcf147f335d4e
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-17 15:55:10 +0000

    some changes

--- project/src/Pages/roadmap/bottom.js
+++ project/src/Pages/roadmap/bottom.js
@@ -8,8 +8,11 @@ const Bottom = (props) => {
         <div>
             <p className="tag body-3">{props.tag}</p>
            <div className="last-line">
-            <div className="up">
-            <p className=" body-3"id="inner-up"><img src="../../assets/shared/icon-arrow-up.svg"/>{props.up}</p>
+              <div className="up">
+                <div className="inner-box-up">
+                <img src="../../assets/shared/icon-arrow-up.svg"/>
+                <p className=" body-3">{props.up}</p>
+              </div>
             </div>
             <p className="comments">{props.comments}</p>
             </div>
