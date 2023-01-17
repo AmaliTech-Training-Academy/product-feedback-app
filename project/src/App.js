3e548dbca5ffe77af8f98dc641f4be5121d54767
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-17 09:59:56 +0000

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button className='button-1-default'>button</button>
-      <button className='button-2-default'>button</button>
-      <button className='button-3-default'>button</button>
-      <button className='button-4-default'>button</button>
-      <button className='go-back'>button</button>
+
     </div>
   );
 }
