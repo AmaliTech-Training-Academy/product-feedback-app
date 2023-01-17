151af69f4997bbe58fba51e361a34b86b321c421
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-17 09:58:53 +0000

    Add base styles

--- project/src/App.js
+++ project/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <button className='button-1-default'>button</button>
+      <button className='button-2-default'>button</button>
+      <button className='button-3-default'>button</button>
+      <button className='button-4-default'>button</button>
+      <button className='go-back'>button</button>
     </div>
   );
 }
