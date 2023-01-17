d9a9e9b1022e56440916458f2a7f158440cba187
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-01-17 13:52:29 +0000

    creatind sidebar components

---
+++ project/src/Components/Sidebar/Components/FrontendMentor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FrontendMentors } from '../SidebarStyles'
+
+function FrontendMentor() {
+  return (
+    <FrontendMentors>
+      <h2>Frontend Mentor</h2>
+      <span className="body-2">Feedback Board</span>
+    </FrontendMentors>
+  )
+}
+
+export default FrontendMentor
