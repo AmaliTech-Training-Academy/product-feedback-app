fcdefb07c60b9a2aed26ab97e5d2d0c0d3284d78
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-17 16:37:52 +0000

    update css on bottom part

--- project/src/Pages/roadmap/bottom.js
+++ project/src/Pages/roadmap/bottom.js
@@ -10,11 +10,14 @@ const Bottom = (props) => {
            <div className="last-line">
               <div className="up">
                 <div className="inner-box-up">
-                <img src="../../assets/shared/icon-arrow-up.svg"/>
-                <p className=" body-3">{props.up}</p>
+                <img className="arrow-up" src="../../assets/shared/icon-arrow-up.svg"/>
+                <p className=" up-votes body-3">{props.up}</p>
               </div>
             </div>
+            <div className="other-line">
+            <img className="comments-svg" src="../../assets/shared/icon-comments.svg"/>
             <p className="comments">{props.comments}</p>
+            </div>
             </div>
         </div>
         </>
