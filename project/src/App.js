bb934e2051b166837b4aeefa16f0a3cdd00899c8
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-17 08:46:55 +0000

    Project setup

---
+++ project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>hey</h4>
+      <h3>hey</h3>
+      <h2>hey</h2>
+
+    </div>
+  );
+}
+
+export default App;
