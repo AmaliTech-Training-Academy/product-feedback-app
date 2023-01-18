c2b4bdf22a82ae5861f8eaa15563bfedc64c5fda
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-18 16:28:16 +0000

    Add edit feedback

--- project/src/App.js
+++ project/src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
-
-import Home from './Pages/Home/Home'; 
-import SuggestionHome from './Pages/Home/SuggestionHome';
+import Header from './Components/Header/Header';
 // import Feedback from './Pages/Feedback/Feedback'
-import Feedback from './Pages/Feedback/Feedback'
 
 function App() {
   return (
     <div className="App">
-      <Home /> 
-      {/* <SuggestionHome /> */}
-
-      <Feedback />
-      <Feedback type='Edit' />
+      {/* <Feedback /> */}
+      <Header />
     </div>
   );
 }
