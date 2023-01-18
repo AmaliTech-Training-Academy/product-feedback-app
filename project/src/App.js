e2846c067eb4f725262a2867f7a0aeb17a76b3a3
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-01-18 12:02:40 +0000

    created the empty component of the suggestion page

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Home />
     </div>
   );
 }
---
+++ project/src/Pages/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './HomeStyles.css'
+
+function Home() {
+  return (
+    <div className='main-page'>
+      {/* Sidebar  */}
+      <section className='sidebar'></section>
+      <div>
+        {/* Header */}
+        <header className='header'></header>
+        <div className='empty-page'>
+          <div className='empty-page-component'>
+            <img src='./assets/suggestions/illustration-empty.svg'/>
+            <div className='text-container'>
+              <span className='h1'>There is no feedback yet.</span>
+              <span className='body-1'>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</span>
+              <button className='button-1-default'>+ Add Feedback</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
