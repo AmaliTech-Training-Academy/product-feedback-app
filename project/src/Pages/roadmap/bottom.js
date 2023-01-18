aedb512c17a2da4d67a0ecee0a6a0e26a8b6e1d5
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-18 08:33:13 +0000

    merge

--- project/src/Pages/roadmap/bottom.js
+++ project/src/Pages/roadmap/bottom.js
@@ -8,7 +8,6 @@ const Bottom = (props) => {
         <div>
             <p className="tag body-3">{props.tag}</p>
            <div className="last-line">
-<<<<<<< HEAD
               <div className="up">
                 <div className="inner-box-up">
                 <img className="arrow-up" src="../../assets/shared/icon-arrow-up.svg"/>
@@ -20,13 +19,6 @@ const Bottom = (props) => {
             <p className="comments">{props.comments}</p>
             </div>
             </div>
-=======
-            <div className="up">
-            <p className=" body-3"id="inner-up"><img src="../../assets/shared/icon-arrow-up.svg"/>{props.up}</p>
-            </div>
-            <p className="comments">{props.comments}</p>
-            </div>
->>>>>>> ec1f0b6c4da2105faf08362423edfe865b312a16
         </div>
         </>
     )
