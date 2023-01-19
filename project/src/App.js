a0b3f6d58a435d1de9db12b9e4914080dcbc1175
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-19 16:31:23 +0000

    Add api calls

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Feedback from './Pages/Feedback/Feedback'
+import EditFeedback from './Pages/Edit Feedback/EditFeedback';
 
 function App() {
   return (
     <div className="App">
-      <Feedback />
-      <Feedback type='Edit' />
+      <EditFeedback />
     </div>
   );
 }
