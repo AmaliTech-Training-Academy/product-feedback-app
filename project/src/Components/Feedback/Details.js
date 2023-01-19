bcccf6606554b08ed490f10ddc851602ec3e25f9
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-19 16:31:23 +0000

    Add api calls

--- project/src/Components/Feedback/Details.js
+++ project/src/Components/Feedback/Details.js
@@ -3,12 +3,15 @@ import {
     Container
 } from './FeedbackStyles'
 
-function Details() {
+function Details(props) {
+  const getDetails = (e) => {
+    props.value(e.target.value)
+  }
   return (
     <Container type='details'>
         <span className='h4 title'>Feedback Details</span>
         <span className='h4 description'>Include any specific comments on what should be improved, added, etc.</span>
-        <textarea type='text' className='input details' />
+        <textarea type='text' className='input details' onChange={getDetails}/>
     </Container>
   )
 }
--- project/src/Pages/Edit Feedback/EditFeedback.js
+++ project/src/Pages/Edit Feedback/EditFeedback.js
@@ -1,30 +1,37 @@
-import React, {useEffect, useState} from 'react'
+// import React, {useEffect, useState} from 'react'
 import Feedback from '../Feedback/Feedback'
-import axios from 'axios'
+// import axios from 'axios'
 
 function EditFeedback() {
-    const [data, setData] = useState('')
-    const [image, setImage] = useState('')
+    // const [data, setData] = useState('')
+    // const [image, setImage] = useState('')
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/currentUser')
-            .then(response => {
-                setData(response.data.name)
-                setImage(response.data.image)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [])
-    // const getRequest = () => {
-
+    // useEffect(() => {
+    //     axios.get('http://localhost:8000/currentUser')
+    //         .then(response => {
+    //             setData(response.data.name)
+    //             setImage(response.data.image)
+    //         })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
+    // }, [])
+    // const postRequest = () => {
+    //     axios({
+    //         method: 'post',
+    //         url: 'http://localhost:8000/currentUser',
+    //         data: {
+    //             name: 'Mikel',
+    //             image: 'bonk'
+    //         }
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
     // }
   return (
     <>
         <Feedback type= 'Edit' />
-        {/* <button onClick={() => getRequest()}>Get</button> */}
-        <span>{data}</span>
-        <img src={image} alt='' />
     </>
   )
 }
