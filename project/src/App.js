bbc3b4e04832359aca0b7482fb4f802285a83e18
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-01-19 16:28:20 +0000

    worked on the comment section

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import FeedbackDetails from './Pages/Home/FeedbackDetails/FeedbackDetails';
+// import Feedback from './Pages/Feedback/Feedback'
+import FeedbackDetails from './Pages/Home/FeedbackDetails/FeedbackDetails';
 
 function App() {
   return (
     <div className="App">
+      {/* <Feedback />
+      <Feedback type='Edit' /> */}
+      <FeedbackDetails/>
       <FeedbackDetails/>
 
       
