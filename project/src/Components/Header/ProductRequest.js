a7910ab3d6abf7f0d7c6a7c89035c8079bf90fef
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-19 10:43:30 +0000

    api handling part 1

--- project/src/Components/Header/ProductRequest.js
+++ project/src/Components/Header/ProductRequest.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ComponentBox from "../../Pages/roadmap/Planned";
 
 
+
 const ProductRequest = ({title, productRequests, subtitle})=>{
+    const [myProduct, fetchMyProduct] = useState([])
+    useEffect(() => {
+        fetch('../data/data.json')
+        .then((res) => res.json())
+        .then((res) => {
+            fetchMyProduct(res)
+            console.log(res);
+        })
+    }, []);
     return(
         <>
          <div className="plan-st">
         <div>          
-            <h3>{`${title}(${productRequests.length})`}</h3>
+            <h3>{`${title}(${myProduct.length})`}</h3>
             <p>{subtitle}</p>
         </div>
-            {productRequests.map((data, key) => {
+            {myProduct.map((data, key) => {
+                console.log(myProduct)
                     return(
                         <ComponentBox key={key}
                         subtitle={data.status}
