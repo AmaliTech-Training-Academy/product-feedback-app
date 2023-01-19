b5fe5e2167cdf02ca2a76449a5814d86cc419a7f
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-19 16:31:23 +0000

    Add api calls

--- project/src/Components/Feedback/UpdateStatus.js
+++ project/src/Components/Feedback/UpdateStatus.js
@@ -5,14 +5,12 @@ import {
 
 
 
-function UpdateStatus() {
+function UpdateStatus(props) {
     
     const [isClicked, setIsClicked] = useState(false)
 
-    const [option, setOption] = useState('Suggestion')
-
     const handleOption = (name) => {
-        setOption(name)
+        props.setDetailOption(name)
         setIsClicked(!isClicked)
     }
   return (
@@ -20,28 +18,28 @@ function UpdateStatus() {
         <span className='h4 title'>Update Status</span>
         <span className='h4 description'>Change feature state</span>
         <div className='input' onClick={() => setIsClicked(!isClicked)}>
-            <span className='body-2 placeholder'>{option}</span>
+            <span className='body-2 placeholder'>{props.detailOption}</span>
             <img className='down-arrow' src={isClicked ? "./assets/shared/icon-arrow-up.svg" : "./assets/shared/icon-arrow-down.svg"} alt='' />
         </div>
         {isClicked && <div className='select'>
             <div className='options' onClick={() => handleOption('Suggestion')}>
                 <span className='body-1 option'>Suggestion</span>
-                {(option === 'Suggestion') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
+                {(props.detailOption === 'Suggestion') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
                 }
             </div>
             <div className='options' onClick={() => handleOption('Planned')}>
                 <span className='body-1 option'>Planned</span>
-                {(option === 'Planned') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
+                {(props.detailOption === 'Planned') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
                 }
             </div>
             <div className='options' onClick={() => handleOption('In-Progress')}>
                 <span className='body-1 option'>In-Progress</span>
-                {(option === 'In-Progress') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
+                {(props.detailOption === 'In-Progress') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
                 }
             </div>
             <div className='options last' onClick={() => handleOption('Live')}>
                 <span className='body-1 option'>Live</span>
-                {(option === 'Live') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
+                {(props.detailOption === 'Live') && <img src='./assets/shared/icon-check.svg' alt='' className='check' />
                 }
             </div>
         </div>}
