c6976e43091346b85f7acb3d9a3b0f43e239849d
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-20 11:34:42 +0000

    Update Suggestions.js

--- project/src/Components/Suggestions/Suggestions.js
+++ project/src/Components/Suggestions/Suggestions.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SuggestionContainer } from './SuggestionStyles'
 import { Enhancement } from '../Sidebar/SidebarStyles'
+import axios from 'axios'
 
 function Suggestions() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/productRequests')
+    .then(res => {
+      setData(res.data )
+    })
+
+  }, [])
   return (
-    <SuggestionContainer>
-      <div className="suggestion-board">
-        <div className="right-components">
-          <div className="top-arrow">
-            <img src="./assets/shared/icon-arrow-up.svg"/>
-            <span>112</span>
+    <>
+      {data && data.map((item) => {
+        return (
+        <SuggestionContainer>
+          <div className="suggestion-board">
+            <div className="right-components">
+              <div className="top-arrow">
+                <img src="./assets/shared/icon-arrow-up.svg" alt=''/>
+                <span>{item.upvotes}</span>
+              </div>
+              <div className="text">
+                <span className='h3'>{item.title}</span>
+                <span className='body-1'>{item.description}</span>
+                <Enhancement>{item.category}</Enhancement>
+              </div>
+            </div>
+            <div className="left-components">
+              <img src="./assets/shared/icon-comments.svg" alt=''/>
+              <span>2</span>
+            </div>
           </div>
-          <div className="text">
-            <span className='h3'> Add tags for solutions.</span>
-            <span className='body-1'> Easier to search for solutions based on a specific stack.</span>
-            <Enhancement>Enhancement</Enhancement>
-          </div>
-        </div>
-        <div className="left-components">
-          <img src="./assets/shared/icon-comments.svg"/>
-          <span>2</span>
-        </div>
-      </div>
-    </SuggestionContainer>
+       </SuggestionContainer>
+        )
+        })
+      }
+    </>
   )
 }
 
