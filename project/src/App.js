b458a7b41269624af94d431bc530ee1bd020111b
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-20 09:56:11 +0000

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import MobileNav from './Components/Header/Mobile nav/MobileNav';
+// import MobileNav from './Components/Header/Mobile nav/MobileNav';
+import Suggestions from './Components/Suggestions/Suggestions';
 // import EditFeedback from './Pages/Edit Feedback/EditFeedback';
-import Feedback from './Pages/Feedback/Feedback';
+// import Feedback from './Pages/Feedback/Feedback';
 
 function App() {
   return (
     <div className="App">
       {/* <EditFeedback /> */}
       {/* <MobileNav /> */}
-      <Feedback />
+      {/* <Feedback /> */}
+      <Suggestions />
     </div>
   );
 }
