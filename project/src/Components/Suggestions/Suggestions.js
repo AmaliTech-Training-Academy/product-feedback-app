b97a97ed88125faf045015aa4446be1f2e0dbdf3
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-20 11:34:42 +0000

    Update Suggestions.js

--- project/src/Components/Suggestions/Suggestions.js
+++ project/src/Components/Suggestions/Suggestions.js
@@ -1,6 +1,8 @@
+import React, { useEffect, useState } from 'react'
 import React, {useState, useEffect} from 'react'
 import { SuggestionContainer } from './SuggestionStyles'
 import { Enhancement } from '../Sidebar/SidebarStyles'
+import axios from 'axios'
 import axios from 'axios'
 import EmptyComponent from '../EmptyComponent/EmptyComponent'
 
@@ -8,40 +10,39 @@ function Suggestions() {
 
   const [data, setData] = useState(null)
 
-  useEffect(()=> {
+  useEffect(() => {
     axios.get('http://localhost:8000/productRequests')
-    .then(response => {
-      console.log(response)
-      setData(response.data)
+    .then(res => {
+      setData(res.data )
     })
+
   }, [])
   return (
     <>
-    {data ? data.map((item) => {
-      return (
+      {data && data.map((item) => {
+        return (
         <SuggestionContainer>
-        <div className="suggestion-board">
-          <div className="right-components">
-            <div className="top-arrow">
-              <img src="./assets/shared/icon-arrow-up.svg"/>
-              <span>{item.upvotes}</span>
+          <div className="suggestion-board">
+            <div className="right-components">
+              <div className="top-arrow">
+                <img src="./assets/shared/icon-arrow-up.svg" alt=''/>
+                <span>{item.upvotes}</span>
+              </div>
+              <div className="text">
+                <span className='h3'>{item.title}</span>
+                <span className='body-1'>{item.description}</span>
+                <Enhancement>{item.category}</Enhancement>
+              </div>
             </div>
-            <div className="text">
-              <span className='h3'>{item.title}</span>
-              <span className='body-1'>{item.description}</span>
-              <Enhancement>{item.category[0].toUpperCase() + item.category.slice(1)}</Enhancement>       
+            <div className="left-components">
+              <img src="./assets/shared/icon-comments.svg" alt=''/>
+              <span>2</span>
             </div>
           </div>
-          <div className="left-components">
-            <img src="./assets/shared/icon-comments.svg"/>
-            <span>{item.comments ? item.comments.length : '0'}</span>
-          </div>
-        </div>
-      </SuggestionContainer>
-      )
-    }) 
-    : <EmptyComponent /> 
-  }
+       </SuggestionContainer>
+        )
+        })
+      }
     </>
   )
 }
