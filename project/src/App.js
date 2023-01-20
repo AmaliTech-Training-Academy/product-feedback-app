3cdb99e9ee1feb6bb4a679e42cbb2a06330afa67
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-01-20 09:52:01 +0000

    added cursor behavior of suggestion board

--- project/src/App.js
+++ project/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Home from './Pages/Home/Home'; 
 import SuggestionHome from './Pages/Home/SuggestionHome';
-// import Feedback from './Pages/Feedback/Feedback'
+import Feedback from './Pages/Feedback/Feedback'
 
 function App() {
   return (
     <div className="App">
-      {/* <Feedback /> */}
       {/* <Home />  */}
       <SuggestionHome />
+      <Feedback />
+      <Feedback type='Edit' />
     </div>
   );
 }
