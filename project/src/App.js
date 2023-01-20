e8e1487f976a0f065917bc3f199bf95d23917515
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-01-20 14:07:29 +0000

    added the suggestion boards

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SuggestionHome from './Pages/Home/SuggestionHome'
 
 function App() {
   return (
     <div className="App">
-    
+      <SuggestionHome />
     </div>
   );
 }
