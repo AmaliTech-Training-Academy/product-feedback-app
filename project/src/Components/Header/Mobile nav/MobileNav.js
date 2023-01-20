ccef94437bc78b1fcd6eb34ffe9e5b6c77f36cd5
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-20 09:29:17 +0000

    Add mobile

---
+++ project/src/Components/Header/Mobile nav/MobileNav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    Container
+} from './MobileNavStyles'
+
+function MobileNav() {
+  return (
+    <Container>
+        hey
+    </Container>
+  )
+}
+
+export default MobileNav
