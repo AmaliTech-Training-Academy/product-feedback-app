3b4f883a787865579e98e9423f4a88df94069c84
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-20 09:29:17 +0000

    Add mobile

--- project/src/Pages/Edit Feedback/EditFeedback.js
+++ project/src/Pages/Edit Feedback/EditFeedback.js
@@ -3,32 +3,6 @@ import Feedback from '../Feedback/Feedback'
 // import axios from 'axios'
 
 function EditFeedback() {
-    // const [data, setData] = useState('')
-    // const [image, setImage] = useState('')
-
-    // useEffect(() => {
-    //     axios.get('http://localhost:8000/currentUser')
-    //         .then(response => {
-    //             setData(response.data.name)
-    //             setImage(response.data.image)
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }, [])
-    // const postRequest = () => {
-    //     axios({
-    //         method: 'post',
-    //         url: 'http://localhost:8000/currentUser',
-    //         data: {
-    //             name: 'Mikel',
-    //             image: 'bonk'
-    //         }
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    // }
   return (
     <>
         <Feedback type= 'Edit' />
