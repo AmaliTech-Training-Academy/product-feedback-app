6f4546260f0ce7c56043b476cb132e8993bbe7fa
MichaelAffanyi <117348769+MichaelAffanyi@users.noreply.github.com>
2023-01-23 12:14:09 -0800

    Form validation

--- project/src/Pages/Feedback/Feedback.js
+++ project/src/Pages/Feedback/Feedback.js
@@ -14,6 +14,10 @@ function Feedback(props) {
   const [detailOption, setDetailOption] = useState('Suggestion')
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
+  const [error, setError] = useState({
+    errorTitle: false,
+    errorDetail: false
+  })
 
   const getTitleValue = (value) => {
     setTitle(value)
@@ -40,6 +44,43 @@ function Feedback(props) {
       .then(response => {
         console.log(response)
       })
+    if(title.length === 0) {
+      setError({
+        errorTitle: true,
+        errorDetail: false
+      })
+    }
+      else if(details.length === 0) {
+        setError({
+          errorTitle: false,
+          errorDetail: true
+        }) 
+      }
+    
+
+      
+          if (props.type === 'Edit') { 
+            axios.patch('http://localhost:8000/productRequests', {
+              title: title,
+              category: option,
+              status: detailOption,
+              description: details
+            })
+            .catch((e) => {
+              console.log(e)
+            })
+          }
+          axios.post('http://localhost:8000/productRequests', {
+            title: title,
+            category: option,
+            upvotes: '0',
+            status: detailOption,
+            description: details,
+            comments: []
+          })
+          .catch((e) => {
+            console.log(e)
+          })
   }
 
   return (
@@ -49,9 +90,11 @@ function Feedback(props) {
           <img src={props.type === 'Edit' ? './assets/shared/icon-edit-feedback.svg' : './assets/shared/icon-new-feedback.svg'} alt='' className='plus' />
           <span className='h1 head'>{props.type === 'Edit' ? 'Editing ‘Add a dark theme option’' : 'Create New Feedback' }</span>
           <FeedbackTitle value= {getTitleValue}/>
+          {error.errorTitle && <span className='h4 error'>Can't be empty</span>}
           <FeedbackCategory option={option} setOption={setOption}/>
           {props.type === 'Edit' && <UpdateStatus detailOption={detailOption} setDetailOption={setDetailOption}/>}
           <Details value={getDetailsValue} />
+          {error.errorDetail && <span className='h4 error'>Can't be empty</span>}
           <div className='buttons'>
             {(props.type === 'Edit') && <button className='button-4-default delete'>Delete</button>}
             <button className='button-3-default cancel'>Cancel</button>
