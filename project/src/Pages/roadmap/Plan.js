5d516678a3b1754f457037b9db139faee1e5d710
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-23 14:45:21 +0000

    creating mobile view

--- project/src/Pages/roadmap/Plan.js
+++ project/src/Pages/roadmap/Plan.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ComponentBox from "./Planned";
 import './roadmap.css'
 import productdata from '../../data/data.json'
-import { useState,useEffect } from "react";
+import {useState,useEffect} from "react";
 import ProductRequest from "../../Components/Roadmap/ProductRequest";
-import TabNavItem from "../../Components/Tab";
-import TabContent from "../../Components/TabContent";
+import TabNavItem from "../../Components/Roadmap/Tab";
+import TabContent from "../../Components/Roadmap/TabContent";
+
 
 
 function Plan(){
@@ -13,9 +13,7 @@ function Plan(){
     const [ProgressRequestProducts, setProgressRequestProduct] = useState([]);
     const [LiveRequestProducts, setLiveRequestProduct] = useState([]);
     const [activeTab, setActiveTab] = useState("tab1");
-
-
-
+   
 
     const groupData=()=>{
         let planned=[]
@@ -26,15 +24,12 @@ function Plan(){
         productdata.productRequests.map((productRequest)=>{
             if(productRequest.status==='planned'){
                 planned.push(productRequest);
-
             }
             if(productRequest.status==='in-progress'){
-                progress.push(productRequest);
-                
+                progress.push(productRequest);   
             }
             if(productRequest.status==='live'){
-                live.push(productRequest);
-                
+                live.push(productRequest);   
             }
         })
 
@@ -47,69 +42,86 @@ function Plan(){
         groupData()
 
     },[])
-  //  Functions to handle Tab Switching
-  const handleTab1 = () => {
-    // update the state to tab1
-    setActiveTab("tab1");
-  };
-  const handleTab2 = () => {
-    // update the state to tab2
-    setActiveTab("tab2");
-  };
-      
-
-    
+ 
     return(
         <>
         <nav>
           <ul>
-            {/* <li className={activeTab === "tab1" ? "active" : ""} >Planned</li>
-            <li className={activeTab === "tab2" ? "active" : ""} >In-Progress</li>
-            <li className={activeTab === "tab3" ? "active" : ""} >Live</li> */}
-            <TabNavItem title="Planned" id="tab1" activeTab={activeTab} setActiveTab={setActiveTab}/>
-            <TabNavItem title="In-Progress" id="tab2" activeTab={activeTab} setActiveTab={setActiveTab}/>
-            <TabNavItem title="Live" id="tab3" activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <TabNavItem title="Planned(2)" id="tab1" activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <TabNavItem title="In-Progress(3)" id="tab2" activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <TabNavItem title="Live(1)" id="tab3" activeTab={activeTab} setActiveTab={setActiveTab}/>
           </ul>
         </nav>
-    
-        <div className="roadmap-main">
-        {/* {activeTab === "tab1" ? <PlannedRequestProducts/> : <ProgressRequestProducts/>} */}
-        <TabContent id="tab1" activeTab={activeTab}>
+    <div className="small-screen">
+        <div className="roadmap-main ">
+     <TabContent id="tab1" activeTab={activeTab}>
       <ProductRequest
-      title="Planned"
-      subtitle="Ideas prioritized for research"
-      productRequests={PlannedRequestProducts}
-      border1="plan"
-      round1="plan-round"
-      /></TabContent>
+        title="Planned"
+        subtitle="Ideas prioritized for research"
+        productRequests={PlannedRequestProducts}
+        border1="plan"
+        round1="plan-round"
+      />
+    </TabContent>
 
      <TabContent id="tab2" activeTab={activeTab}>
       <ProductRequest
-      id="tab2" 
-      activeTab={activeTab}
-      title="In-Progress"
-      subtitle="Currently being developed"
-      productRequests={ProgressRequestProducts}
-      border1="progress"
-      round1="progress-round"
-      /></TabContent>
-
-        <TabContent id="tab3" activeTab={activeTab}>
-       <ProductRequest
-       id="tab3" 
-       activeTab={activeTab}
-      title="Live"
-      subtitle="Released features"
-      productRequests={LiveRequestProducts}
-      border1="live"
-      round1="live-round"/>
+        id="tab2" 
+        activeTab={activeTab}
+        title="In-Progress"
+        subtitle="Currently being developed"
+        productRequests={ProgressRequestProducts}
+        border1="progress"
+        round1="progress-round"
+      />
       </TabContent>
 
-      
-     
-</div>
+      <TabContent id="tab3" activeTab={activeTab}>
+       <ProductRequest
+         id="tab3" 
+         activeTab={activeTab}
+         title="Live"
+         subtitle="Released features"
+         productRequests={LiveRequestProducts}
+         border1="live"
+         round1="live-round"/>
+      </TabContent>      
+      </div>
+    </div>
+
+
+        <div className="big-screen"> 
+      <div className="roadmap-main">
+     <ProductRequest
+     title="Planned"
+     subtitle="Ideas prioritized for research"
+     productRequests={PlannedRequestProducts}
+     border1="plan"
+     round1="plan-round"
+     />
+     <ProductRequest
+     id="tab2" 
+     activeTab={activeTab}
+     title="In-Progress"
+     subtitle="Currently being developed"
+     productRequests={ProgressRequestProducts}
+     border1="progress"
+     round1="progress-round"
+     />
+     <ProductRequest
+      id="tab3" 
+      activeTab={activeTab}
+     title="Live"
+     subtitle="Released features"
+     productRequests={LiveRequestProducts}
+     border1="live"
+     round1="live-round"/>      
+         </div>
+      </div>
+
         </>
     );
+    
 }
 
 export default Plan
