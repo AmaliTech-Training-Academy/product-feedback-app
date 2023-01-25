664000a820b30c32f62a698a52c025e69db6bbfa
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-25 14:40:49 +0000

    Add navigations

--- project/src/Pages/Home/FeedbackDetails/FeedbackDetails.js
+++ project/src/Pages/Home/FeedbackDetails/FeedbackDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./FeedbackDetails.css";
 import axios from "axios";
 import AddComment from "./AddComment";
