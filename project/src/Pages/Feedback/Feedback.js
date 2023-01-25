6a97b4f418fcbd001af88cb354f5ba3294326412
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-25 14:40:49 +0000

    Add navigations

--- project/src/Pages/Feedback/Feedback.js
+++ project/src/Pages/Feedback/Feedback.js
@@ -70,7 +70,7 @@ function Feedback(props) {
 
   return (
     <Container type={props.type}>
-        <Head />
+        <Link to='/'><Head /></Link>
         <form className='content' onSubmit={handleSubmit}>
           <img src={props.type === 'Edit' ? './assets/shared/icon-edit-feedback.svg' : './assets/shared/icon-new-feedback.svg'} alt='' className='plus' />
           <span className='h1 head'>{props.type === 'Edit' ? 'Editing ‘Add a dark theme option’' : 'Create New Feedback' }</span>
