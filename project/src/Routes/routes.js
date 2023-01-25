fed5fde2c0b42dabb461d7ead2267e79e25fa8dc
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-25 11:17:50 +0000

    linking routes to components

--- project/src/Routes/routes.js
+++ project/src/Routes/routes.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Feedback from "../Pages/Feedback/Feedback";
 import Home from "../Pages/Home/Home";
+import EditFeedback from "../Pages/Edit Feedback/EditFeedback";
+import SuggestionHome from "../Pages/Home/SuggestionHome";
+import FeedbackDetails from "../Pages/Home/FeedbackDetails/FeedbackDetails";
+import { RoadMap } from "../Components/Sidebar/SidebarStyles";
 
 
 function ProductApp(){
@@ -9,6 +14,11 @@ function ProductApp(){
         <>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/suggestion" element={<SuggestionHome/>}/>
+            <Route path="/feedback-detail" element={<FeedbackDetails/>}/>
+            <Route path="/new-feedback" element={<Feedback/>}/>
+            <Route path="/edit-feedback" element={<EditFeedback/>}/>
+            <Route path="/roadmap" element={<RoadMap/>}/>
         </Routes>
         </>
 
