25cb32291588966e616bcedd57f20b868c0135af
aboagyewaa1 <ewurama.amponsah@amalitech.com>
2023-01-25 10:18:38 +0000

    creating routes

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+import ProductApp from './Routes/routes';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <ProductApp/>
+
+      </BrowserRouter>
 
     </div>
   );
