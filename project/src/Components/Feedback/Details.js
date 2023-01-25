85013e70f5ce4a18326a7e32bf4cec5bacf876cb
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-01-25 11:12:00 +0000

    update

--- project/src/Components/Feedback/Details.js
+++ project/src/Components/Feedback/Details.js
@@ -3,15 +3,15 @@ import {
     Container
 } from './FeedbackStyles'
 
-function Details(props) {
+function Details({value, error}) {
   const getDetails = (e) => {
-    props.value(e.target.value)
+    value(e.target.value)
   }
   return (
-    <Container type='details'>
+    <Container type='details' error={error}>
         <span className='h4 title'>Feedback Details</span>
         <span className='h4 description'>Include any specific comments on what should be improved, added, etc.</span>
-        <textarea type='text' className='input details' onChange={getDetails} onClick={() => props.setDetailsError(false)}/>
+        <textarea type='text' className='input details' onChange={getDetails}/>
     </Container>
   )
 }
