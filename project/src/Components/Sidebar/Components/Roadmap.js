8d0c1243dfa14f42e535e72849121a6fff18ce38
Ahmed39-wq <abulfail.rufai@amalitech.com>
2023-01-26 23:59:38 -0800

    add tags and roadmaps functionality

--- project/src/Components/Sidebar/Components/Roadmap.js
+++ project/src/Components/Sidebar/Components/Roadmap.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RoadMap } from '../SidebarStyles'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 
 function Roadmap() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8000/productRequests')
+    .then(res => {
+      const dataWanted = res.data.slice(6);
+      const dataWantedCounts = {};
+      dataWanted.forEach((elt) => {
+        if (elt.status.toLowerCase() !== 'suggestion') {
+          console.log(Object.keys(dataWantedCounts))
+          if (Object.keys(dataWantedCounts).includes(elt.status)) {
+            dataWantedCounts[elt.status] += 1
+          } else {
+            dataWantedCounts[elt.status] = 1;
+          }
+        }
+      });
+      console.log(dataWantedCounts)
+      setData(dataWantedCounts);
+    })
+  }, []);
+
   return (
     <RoadMap>
       <div className='roadmap-container'>
@@ -18,21 +40,21 @@ function Roadmap() {
             <span className='dot'></span>
             <span className='body-1'> Planned</span>
           </div>
-          <span className='num'>2</span>
+          <span className='num'>{data?.planned || 0}</span>
         </div>
         <div className='roadmap-progress'>
           <div>
             <span className='dot'></span>
             <span className='body-1'> In-progress</span>
           </div>
-          <span className='num'>3</span>
+          <span className='num'>{data['in-progress'] || 0}</span>
         </div>
         <div className='roadmap-live'>
           <div>
             <span className='dot'></span>
             <span className='body-1'> Live</span>
           </div>
-          <span className='num'>1</span>
+          <span className='num'>{data?.live || 0}</span>
         </div>
       </div>
     </RoadMap>
